f6d60e4e5a4e6742e2fb71d5444897f0107f05df
fake150907 <yungayeon223@gmail.com>
2024-05-14 11:23:55 +0900

    프로젝트 세팅

---
+++ src/main/java/com/koreait/rest_api_2024_05/RestApi202405Application.java
@@ -0,0 +1,13 @@
+package com.koreait.rest_api_2024_05;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApi202405Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestApi202405Application.class, args);
+    }
+
+}
