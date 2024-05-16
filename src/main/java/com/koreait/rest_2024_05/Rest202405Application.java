72a7f0b51209790337578ece1ea763b70a35ca7b
fake150907 <yungayeon223@gmail.com>
2024-05-16 13:43:31 +0900

    gradle 버전 변경, 스프링 시큐리티 설정 끄기

--- src/main/java/com/koreait/rest_2024_05/Rest202405Application.java
+++ src/main/java/com/koreait/rest_2024_05/Rest202405Application.java
@@ -1,13 +1,13 @@
-package com.koreait.rest_api_2024_05;
+package com.koreait.rest_2024_05;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RestApi202405Application {
+public class Rest202405Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(RestApi202405Application.class, args);
+        SpringApplication.run(Rest202405Application.class, args);
     }
 
 }
